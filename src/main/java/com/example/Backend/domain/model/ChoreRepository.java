a1d073350f7bdc066e64cafe9c3a988efdc6c954
AliAl-Gburi <78793598+AliAl-Gburi@users.noreply.github.com>
2022-04-21 15:30:13 +0200

    somestuff

--- src/main/java/com/example/Backend/domain/model/ChoreRepository.java
+++ src/main/java/com/example/Backend/domain/model/ChoreRepository.java
@@ -1,7 +1,9 @@
 package com.example.Backend.domain.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChoreRepository extends JpaRepository<Chore, Long> {
     
 }
--- src/main/java/com/example/Backend/domain/model/MessageRepository.java
+++ src/main/java/com/example/Backend/domain/model/MessageRepository.java
@@ -1,6 +1,8 @@
 package com.example.Backend.domain.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 }
